6d077bb7494d193f659a397e59a25a9b6c5eab4e
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-09-10 00:50:57 +0000

    test: improve cases

--- __tests__/sort-numbers.spec.ts
+++ __tests__/sort-numbers.spec.ts
@@ -1,17 +1,17 @@
 import { sortNumbersAscending, sortNumbersDescending } from '@src/sort-numbers'
 
 test('sortNumbersAscending', () => {
-  const arr: number[] = [3, 2, 10]
+  const arr: number[] = [3, 2, 10, 2]
 
   sortNumbersAscending(arr)
 
-  expect(arr).toStrictEqual([2, 3, 10])
+  expect(arr).toStrictEqual([2, 2, 3, 10])
 })
 
 test('sortNumbersDescending', () => {
-  const arr: number[] = [3, 2, 10]
+  const arr: number[] = [3, 2, 10, 2]
 
   sortNumbersDescending(arr)
 
-  expect(arr).toStrictEqual([10, 3, 2])
+  expect(arr).toStrictEqual([10, 3, 2, 2])
 })
--- __tests__/sort-strings.spec.ts
+++ __tests__/sort-strings.spec.ts
@@ -1,17 +1,17 @@
 import { sortStringsAscending, sortStringsDescending } from '@src/sort-strings'
 
 test('sortStringsAscending', () => {
-  const arr: string[] = ['10', '3', '2',]
+  const arr: string[] = ['10', '2', '3', '2']
 
   sortStringsAscending(arr)
 
-  expect(arr).toStrictEqual(['10', '2', '3'])
+  expect(arr).toStrictEqual(['10', '2', '2', '3'])
 })
 
 test('sortStringsDescending', () => {
-  const arr: string[] = ['10', '3', '2']
+  const arr: string[] = ['10', '2', '3', '2']
 
   sortStringsDescending(arr)
 
-  expect(arr).toStrictEqual(['3', '2', '10'])
+  expect(arr).toStrictEqual(['3', '2', '2', '10'])
 })
