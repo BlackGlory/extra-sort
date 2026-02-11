0f837f69cc3d086a495f279e28c994c73e58f1cd
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-11 18:55:04 +0000

    perf(sort-numbers-descending): fix benchmark cases

--- benches/sort-numbers-descending.ts
+++ benches/sort-numbers-descending.ts
@@ -36,9 +36,9 @@ benchmark.addCase('.sort(compare)', () => {
 
   function compare(a: number, b: number): number {
     if (a > b) {
-      return 1
-    } else if (a < b) {
       return -1
+    } else if (a < b) {
+      return 1
     } else {
       return 0
     }
